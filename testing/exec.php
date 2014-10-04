2ef47babc8fc24a385822136e7a4796b7e808e4f
als44 <als4462@truman.edu>
2014-10-04 18:19:29 -0500

    added list of exec
    all members of exec are added

--- testing/exec.php
+++ testing/exec.php
@@ -8,20 +8,138 @@ require_once ('session.php');
 </head>
 
 <body>
-    <!-- Javascript method to include navigation -->
-    <nav id="nav" role="navigation"><?php include 'nav.php';?></nav>
-    <!-- PHP method to include navigation -->
+    <nav id="nav" role="navigation"></nav>
+    <div id="header"></div>
+    <div class="row">
+        <div class="large-10 medium-9 small-12 column large-centered medium-centered">
 
-    <!-- Javascript method to include header -->
-    <div id="header"><?php include 'header.php';?></div>
-    <!-- PHP method to include header -->
-
+    <h1> <b> President </b> </h1>
+<p> Matthew Karner <br>
+apo.epsilon.president@gmail.com </p>
+<hr>
+<h2> <b>VP of Large Service </b> </h2>
+<p> Kelsey Mengwasser <br>
+apo.epsilon.largeservice@gmail.com </p>
+<hr>
+<h2> <b>VP of Regular Service</b> </h2>
+<p> Paige Hamilton <br>
+apo.epsilon.regularservice@gmail.com </p>
+<hr>
+<h2> <b> VP of Membership</b> </h2>
+<p> Austin Kerns <br>
+apo.epsilon.membership@gmail.com </p>
+<hr>
+<h2> <b> VP of Pledging </b> </h2>
+<p> Kelley Keim <br>
+apo.epsilon.pledging@gmail.com </p>
+<hr>
+<h2> <b> Sergeant at Arms </b> </h2>
+<p>Riley Williams <br>
+apo.epsilon.sergeant@gmail.com </p>
+<hr>
+<h2> <b> Public Relations </b> </h2>
+<p> Trista Sullivan <br>
+apo.epsilon.pr@gmail.com </p>
+<hr>
+<h2> <b> Recording Secretary </b> </h2>
+<p> Rose Hatting <br>
+apo.epsilon.recsec@gmail.com </p>
+<hr>
+<h2> <b> Treasurer </b> </h2>
+<p> Katie Schmitz <br>
+apo.epsilon.treasurer@gmail.com </p>
+<hr>
+<h2> <b> Scouting Chairman </b> </h2>
+<p> Marissa Fabbri <br>
+apo.epsilon.scouting@gmail.com </p>
+<hr>
+<h2> <b> Brotherhood Chairman </b> </h2>
+ <p> Shelby miller </p>
+ <hr>
+<h2> <b> Alumni Secretary </b> </h2> 
+<p> Matthew LaPointe <br>
+apo.epsilon.alumni@gmail.com </p>
+<hr>
+<h2> <b> Chaplain </b> </h2> 
+<p> Emese Mattingly <br>
+apo.epsilon.chaplain@gmail.com </p>
+<hr>
+<h2> <b> Historian </b> </h2>
+<p> Heidi Glickert <br>
+apo.epsilon.historian@gmail.com <br>
+Katie Kerkemeyer <br>
+apo.epsilon.historian@gmail.com </p>
+<hr>
+<h2> <b> Fundraising Chairman </b> </h2>
+<p> Ashley Banze <br>
+apo.epsilon.fundraising@gmail.com <br>
+Rebecca Atchley <br>
+apo.epsilon.fundraising@gmail.com <br>
+Jessica Wooldridge <br>
+apo.epsilon.fundraising@gmail.com </p>
+<hr>
+<h2> <b> Formal Chairman </b> </h2>
+<p>Taylor Demien <br>
+apo.epsilon.formal@gmail.com <br>
+Katie Winterowd <br>
+apo.epsilon.formal@gmail.com </p>
+<hr>
+<h2> <b> Inter-Chapter Relations Chairman </b> <h2>
+<p> Valerie Frerichs <br>
+apo.epsilon.icr@gmail.com <br>
+David Roach <br>
+apo.epsilon.icr@gmail.com <br>
+Danika Rodriguez <br>
+apo.epsilon.icr@gmail.com </p>
+<hr>
+<h1> <b> Webmaster </b> </h1>
+<p> Dale Race <br>
+apo.epsilon.webmaster@gmail.com <br>
+Austin Stuart <br>
+apo.epsilon.webmaster@gmail.com </p>
+<hr>
+<h2> <b> Red Cross Liaison </b> </h2>
+<p> Jessica Diermann <br>
+apo.epsilon.redcross@gmail.com <br>
+Kristen Womble <br>
+apo.epsilon.redcross@gmail.com </p>
+<hr>
+<h2> <b> Philanthropy </b> </h2>
+<p> Callie Templeton <br> 
+apo.epsilon.philanthropy@gmail.com <br>
+Jordyn Williams <br>
+apo.epsilon.philanthropy@gmail.com <br>
+Molly Turner <br>
+apo.epsilon.philanthropy@gmail.com <br>
+Libby Revelle <br>
+apo.epsilon.philanthropy@gmail.com <br>
+Jamie Haenni <br>
+apo.epsilon.philanthropy@gmail.com </p>
+<hr>
+<h2> <b> Communications Secretary </b> </h2>
+<p> Lucy Bowe <br>
+apo.epsilon.comsec@gmail.com </p> 
+<hr>
+<h2> <b> Homecoming Chairman </b> </h2>
+<p> Alyssa Talkington <br>
+Emma Westin </p>
+<hr>
+<h2> <b> Advisor Liaison </b> </h2>
+<p> Rosalie Swingle </p>
+<hr>
+<h2> <b> Rush Chairman </b> </h2>
+<p>Connor Madden <br>
+apo.epsilon.rushchair@gmail.com <br>
+Rachel Rigsby <br>
+apo.epsilon.rushchair@gmail.com <br>
+Trent Hoover <br>
+apo.epsilon.rushchair@gmail.com <br>
+Mario Castellanos <br>
+apo.epsilon.rushchair@gmail.com <br>
+</p>
+   
 <div class="row">
         <div class="large-10 medium-9 small-12 column large-centered medium-centered">
-            <br>
-            <p>Under construction
-            <br>
-            </p>
 
         </div>
     </div>
