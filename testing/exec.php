c5fcdae3e5e0915662a2526ec22574cea3d4faba
Dale <race.dale@gmail.com>
2014-10-05 11:34:55 -0500

    changed h2 to h1 and removed <b> tags

--- testing/exec.php
+++ testing/exec.php
@@ -12,71 +12,71 @@ require_once ('session.php');
     <div id="header"></div>
     <div class="row">
         <div class="large-6 medium-9 small-12 columns">
-            <h1> <b> President </b> </h1>
+            <h1>  President  </h1>
                 <p> Matthew Karner <br>
                 <a href="mailto:apo.epsilon.president@gmail.com">apo.epsilon.president@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b>VP of Large Service </b> </h2>
+            <h1> VP of Large Service  </h1>
                 <p> Kelsey Mengwasser <br>
                 <a href="mailto:apo.epsilon.largeservice@gmail.com">apo.epsilon.largeservice@gmail.com</a></p>
             <hr>
-            <h2> <b>VP of Regular Service</b> </h2>
+            <h1> VP of Regular Service </h1>
                 <p> Paige Hamilton <br>
                 <a href="mailto:apo.epsilon.regularservice@gmail.com">apo.epsilon.regularservice@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> VP of Membership</b> </h2>
+            <h1>  VP of Membership </h1>
                 <p> Austin Kerns <br>
                 <a href="mailto:apo.epsilon.membership@gmail.com">apo.epsilon.membership@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> VP of Pledging </b> </h2>
+            <h1>  VP of Pledging  </h1>
                 <p> Kelley Keim <br>
                 <a href="mailto:apo.epsilon.pledging@gmail.com">apo.epsilon.pledging@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Sergeant at Arms </b> </h2>
+            <h1>  Sergeant at Arms  </h1>
                 <p>Riley Williams <br>
                 <a href="mailto:apo.epsilon.sergeant@gmail.com">apo.epsilon.sergeant@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Public Relations </b> </h2>
+            <h1>  Public Relations  </h1>
                 <p> Trista Sullivan <br>
                 <a href="mailto:apo.epsilon.pr@gmail.com">apo.epsilon.pr@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Recording Secretary </b> </h2>
+            <h1>  Recording Secretary  </h1>
                 <p> Rose Hatting <br>
                 <a href="mailto:apo.epsilon.recsec@gmail.com">apo.epsilon.recsec@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Treasurer </b> </h2>
+            <h1>  Treasurer  </h1>
                 <p> Katie Schmitz <br>
                 <a href="mailto:apo.epsilon.treasurer@gmail.com">apo.epsilon.treasurer@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Scouting </b> </h2>
+            <h1>  Scouting  </h1>
                 <p> Marissa Fabbri <br>
                 <a href="mailto:apo.epsilon.scouting@gmail.com">apo.epsilon.scouting@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Brotherhood </b> </h2>
+            <h1>  Brotherhood  </h1>
                  <p> Shelby miller
                  <a href="mailto:apo.epsilon.broho@gmail.com">apo.epsilon.broho@gmail.com</a>
                  </p>
              <hr>
-            <h2> <b> Alumni Secretary </b> </h2>
+            <h1>  Alumni Secretary  </h1>
                 <p> Matthew LaPointe <br>
                 <a href="mailto:apo.epsilon.alumni@gmail.com">apo.epsilon.alumni@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Chaplain </b> </h2>
+            <h1>  Chaplain  </h1>
                 <p> Emese Mattingly <br>
                 <a href="mailto:apo.epsilon.chaplain@gmail.com">apo.epsilon.chaplain@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Historian </b> </h2>
+            <h1>  Historian  </h1>
                 <p> Heidi Glickert <br>
                 Katie Kerkemeyer <br>
                 <a href="mailto:apo.epsilon.historian@gmail.com">apo.epsilon.historian@gmail.com</a>
@@ -85,39 +85,39 @@ require_once ('session.php');
         </div>
 
         <div class="large-6 medium-9 small-12 columns">
-            <h2> <b> Fundraising </b> </h2>
+            <h1>  Fundraising  </h1>
                 <p> Ashley Banze <br>
                 Rebecca Atchley <br>
                 Jessica Wooldridge <br>
                 <a href="mailto:apo.epsilon.fundraising@gmail.com">apo.epsilon.fundraising@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Formal </b> </h2>
+            <h1>  Formal  </h1>
                 <p>Taylor Demien <br>
                 Katie Winterowd <br>
                 <a href="mailto:apo.epsilon.formal@gmail.com">apo.epsilon.formal@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Inter-Chapter Relations </b> </h2>
+            <h1>  Inter-Chapter Relations  </h1>
                 <p> Valerie Frerichs <br>
                 David Roach <br>
                 Danika Rodriguez <br>
                 <a href="mailto:apo.epsilon.icr@gmail.com">apo.epsilon.icr@gmail.com</a>
                 </p>
             <hr>
-            <h1> <b> Webmaster </b> </h1>
+            <h1>  Webmaster  </h1>
                 <p> Dale Race <br>
                 Austin Stuart <br>
                 <a href="mailto:apo.epsilon.webmaster@gmail.com">apo.epsilon.webmaster@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Red Cross Liaison </b> </h2>
+            <h1>  Red Cross Liaison  </h1>
                 <p> Jessica Diermann <br>
                 Kristen Womble <br>
                 <a href="mailto:apo.epsilon.redcross@gmail.com">apo.epsilon.redcross@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Philanthropy </b> </h2>
+            <h1>  Philanthropy  </h1>
                 <p> Callie Templeton <br>
                 Jordyn Williams <br>
                 Molly Turner <br>
@@ -126,23 +126,23 @@ require_once ('session.php');
                 <a href="mailto:apo.epsilon.philanthropy@gmail.com">apo.epsilon.philanthropy@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Communications Secretary </b> </h2>
+            <h1>  Communications Secretary  </h1>
                 <p> Lucy Bowe <br>
                 <a href="mailto:apo.epsilon.comsec@gmail.com">apo.epsilon.comsec@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Homecoming </b> </h2>
+            <h1>  Homecoming  </h1>
                 <p> Alyssa Talkington <br>
                 Emma Westin <br>
                 <a href="mailto:apo.epsilon.homecoming@gmail.com">apo.epsilon.homecoming@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Advisor Liaison </b> </h2>
+            <h1>  Advisor Liaison  </h1>
                 <p> Rosalie Swingle <br>
                 <a href="mailto:apo.epsilon.advisorliasion@gmail.com">apo.epsilon.advisorliasion@gmail.com</a>
                 </p>
             <hr>
-            <h2> <b> Rush </b> </h2>
+            <h1>  Rush  </h1>
                 <p>Connor Madden <br>
                 Rachel Rigsby <br>
                 Trent Hoover <br>
