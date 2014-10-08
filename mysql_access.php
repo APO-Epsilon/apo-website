6e49a6cf60c443e805ad3a7540c70d9d1e9caf0d
Dale <race.dale@gmail.com>
2014-10-08 17:17:25 -0500

    New format for quizzing

--- mysql_access.php
+++ mysql_access.php
@@ -1,14 +1,11 @@
 <?php
 
-$db = mysql_connect("mysql.truman.edu", "apo", "glueallE17");
-if (!$db) {
-    	print "Error - Could not connect to mysql";
-    	exit;
-}
+define('DB_HOST', 'mysql.truman.edu');
+define('DB_NAME', 'apo');
+define('DB_USER','apo');
+define('DB_PASSWORD','glueallE17');
 
-$er = mysql_select_db("apo");
-if (!$er) {
-    	print "Error - Could not select database";
-    	exit;
-}
+$db=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+$er=mysql_select_db(DB_NAME,$db) or die("Failed to connect to MySQL: " . mysql_error());
+
 ?>
