757048283d365f0b78c77565d1c7f701a5100849
Dale <race.dale@gmail.com>
2014-11-03 23:06:19 -0600

    Change database info for new server

--- mysql_access.php
+++ mysql_access.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'mysql.truman.edu');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'apo');
 define('DB_USER','apo');
-define('DB_PASSWORD','glueallE17');
+define('DB_PASSWORD','alphaphiomega');
 
 $db=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $er=mysql_select_db(DB_NAME,$db) or die("Failed to connect to MySQL: " . mysql_error());
