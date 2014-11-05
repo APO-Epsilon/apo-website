281c736d3ebbe9d23c63db056f1bdfaf662ac77f
Dale <race.dale@gmail.com>
2014-11-04 23:22:32 -0600

    Only show if logged in

--- risk_management_quiz.php
+++ risk_management_quiz.php
@@ -32,14 +32,17 @@ require_once ('mysql_access.php');
         }
     }
 }
-if (passed_quiz()) {
-    echo "<h2>You have passed the quiz!</h2>";
-} else {
-    echo "<h1>You have <b>NOT</b> passed the quiz!</h1>";
-}?>
+if (!isset($_SESSION['sessionID'])) {
 
-<?php $response=mysql_query("SELECT * FROM questions");?>
+        echo "<p>You need to login before you can see the rest of this section.</p>";
+}else{
+    if (passed_quiz()) {
+        echo "<h2>You have passed the quiz!</h2>";
+    } else {
+        echo "<h1>You have <b>NOT</b> passed the quiz!</h1>";
 
+$response=mysql_query("SELECT * FROM questions");
+echo <<<END
 <br>
 <form method='post' id='quiz_form'>
     <?php while($result=mysql_fetch_array($response)){ ?>
@@ -66,6 +69,9 @@ if (passed_quiz()) {
 </form>
 <div id='result'>
 </div>
+END;
+}
+}?>
 <script src="./js/watch.js"></script>
 <script src="./js/quiz.js"></script>
     <!-- Javascript method to include footer -->
