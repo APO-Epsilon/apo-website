0316a435416e7f2caab53389af870746fbb28f9d
Dale <race.dale@gmail.com>
2014-11-25 22:15:04 -0600

    make mysqli variables global

--- mysql_access.php
+++ mysql_access.php
@@ -3,13 +3,13 @@
 // http://codular.com/php-mysqli
 // http://www.pontikis.net/blog/how-to-use-php-improved-mysqli-extension-and-why-you-should
 error_reporting(-1);
-$DBServer = 'apo.cxav86kuligx.us-west-2.rds.amazonaws.com';
-$DBUser   = 'apo';
-$DBPass   = 'alphaphiomega';
-$DBName   = 'apo';
+global $DBServer = 'apo.cxav86kuligx.us-west-2.rds.amazonaws.com';
+global $DBUser   = 'apo';
+global $DBPass   = 'alphaphiomega';
+global $DBName   = 'apo';
 
 // Object oriented way (good way)
-$db = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+global $db = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
 
 // check connection
 if ($db->connect_error) {
