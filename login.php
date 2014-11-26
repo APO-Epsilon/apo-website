25fc2cdb3f00a03ad17a046fdd80d0470ccaaf9c
Dale <race.dale@gmail.com>
2014-11-25 23:46:26 -0600

    remove db parameter

--- login.php
+++ login.php
@@ -90,7 +90,7 @@ function process_login(){
 			if ($hasher->CheckPassword($password, $hash)) {
 				$what = 'Authentication succeeded';
 				$select = "SELECT * FROM contact_information WHERE username=`$username`";
-				$query = $db->query($db, $select) or die("Unable to get data.");
+				$query = $db->query($select) or die("Unable to get data.");
 				$r = $query->fetch_assoc();
 			} else {
 				$what = 'Authentication failed.  Please try again.';
