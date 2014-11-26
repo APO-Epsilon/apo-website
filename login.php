ed9f63403db4d063e486bbdf24658bbd31364eb5
Dale <race.dale@gmail.com>
2014-11-26 00:09:48 -0600

    stmt close()

--- login.php
+++ login.php
@@ -89,6 +89,7 @@ function process_login(){
 
 			if ($hasher->CheckPassword($password, $hash)) {
 				$what = 'Authentication succeeded';
+				$stmt->close();
 				$select = "SELECT * FROM contact_information WHERE username=`$username`";
 				$query = $db->query($select) or die("Unable to get data. $db->error");
 				$r = $query->store_result();
