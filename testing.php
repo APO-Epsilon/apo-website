7e6193307bb6f2d0c660bf45da3fd22cf34a109c
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2014-11-28 01:37:05 -0600

    Update testing.php

--- testing.php
+++ testing.php
@@ -18,7 +18,7 @@ require_once ('mysql_access.php');
     <!-- PHP method to include header -->
 <?php
 function whoami() {
-	echo 'a $_SESSION['sessionID']';
+	echo "<p>Exec:", $_SESSION['sessionID'], "</p>";
 }
 ?>
 
