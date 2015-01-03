1caa8a9031593d0e35c3e19fa790edfe7acf8544
Dale <race.dale@gmail.com>
2015-01-03 12:34:04 -0600

    Round parallax vars to 2 places for performance

--- js/parallax.js
+++ js/parallax.js
@@ -18,10 +18,12 @@ $(document).ready(function(){
                     
 		// Scroll the background at var speed
 		// the yPos is a negative value because we're scrolling it UP!								
-		var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-		
-		// Put together our final background position
-		var coords = '50% '+ yPos/1.5 + 'px';
+		var yMath = -($window.scrollTop() / ($bgobj.data('speed')) * 1.5);
+        var yPos = +(Math.round(yMath + "e+" + 2)  + "e-" + 2);
+        //var yPos = round(yMath,2);
+
+        // Put together our final background position
+		var coords = '50% '+ yPos + 'px';
 
 		// Move the background
 		$bgobj.css({ backgroundPosition: coords });
