46a0dee9d3d2b29e2963f72362d033bff1620970
Nicholas Greer Eaton <nge4445@truman.edu>
2015-02-05 17:39:59 -0600

    Added disclaimer

--- join.php
+++ join.php
@@ -79,6 +79,16 @@ echo "</ul>";
     No. APO is not exclusive in its recruitment process, so anyone with the desire to serve is encouraged to join.  As long as you complete the rush requirements (usually three service events and three fellowship events), you will be invited to join the organization.</p>
        </div>
     </div>
+    <p>
+This electronic document is intended for public viewing and is solely for personal reference. It should not be considered an authoritative source nor an official publication of Alpha Phi Omega. Inquiries regarding Alpha Phi Omega and its official publications may be directed to: 
+
+Alpha Phi Omega
+14901 E. 42nd Street
+Independence, MO 64055
+
+Alpha Phi Omega is a copyrighted, registered trademark in the USA.
+All rights reserved.</p>
+</div>
     <div id="footer"></div>
 </body>
 </html>
