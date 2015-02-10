8ecb85ef9407d90a61ade2afb951ccdeab2729bd
Nicholas Greer Eaton <nge4445@truman.edu>
2015-02-09 18:37:02 -0800

    Update actives.php

--- actives.php
+++ actives.php
@@ -20,7 +20,6 @@ require_once ('session.php');
         <div class="large-10 medium-9 small-12 column large-centered medium-centered">
             <p>
              </p>
-            </p>
         </div>
     </div>
     <!-- Javascript method to include footer -->
