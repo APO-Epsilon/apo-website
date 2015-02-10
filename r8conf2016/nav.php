c944f1f585b4456733b51ece0f7bea5744895193
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-10 13:42:36 -0600

    Update nav.php

--- r8conf2016/nav.php
+++ r8conf2016/nav.php
@@ -3,47 +3,8 @@
 	<a href="#" title="Hide navigation">Hide</a>
 	<ul class="clearfix">
 		<li><a href="index.php">Home</a></li>
-		<li>
-			<a href="about.php" aria-haspopup="true"><span>About</span></a>
-			<ul>
-				<li><a href="join.php">Join</a></li>
-				<li><a href="history.php">History</a></li>
-				<li><a href="dates.php">Important Dates</a></li>
-				<li><a href="awards.php">Awards</a></li>
-                <li><a href="traditions.php">Traditions</a></li>
-			</ul>
-		</li>
-		<li>
-			<a href="?work" aria-haspopup="true"><span>Our Members</span></a>
-			<ul>
-				<li><a href="exec.php">Executive Board</a></li>
-				<li><a href="actives.php">Actives</a></li>
-				<li><a href="alumni.php">Alumni</a></li>
-                <li><a href="advisors.php">Advisors</a></li>
-			</ul>
-		</li>
-		<li>
-			<a href="?active" aria-haspopup="true"><span>Active</span></a>
-            <ul>
-            		<li><a href="login.php">Login/Logout</a></li>
-                    <li><a href="attendance.php">Attendance</a></li>
-                    <li><a href="https://docs.google.com/document/d/15-J7-iN9vKM0x3l0wS_EtOiuRgd-lAal-sa-UomSXBY">Bylaws</a></li>
-                    <li><a href="calendar.php">Calendar</a></li>
-                    <li><a href="service_hours.php">Hours</a></li>
-                    <li><a href="risk_management_quiz.php">Risk Management Quiz</a></li>
-                    <li><a href="members_list.php">Members Info</a></li>
-                    <li><a href="updateinfo.php">Update Info</a></li>
-            </ul>
-        </li>
-		<li>
-            <a href="?exec" aria-haspopup="true"><span>Executive</span></a>
-                <ul>
-                    <li><a href="attendance_admin.php">Record Attendance</a></li>
-                    <li><a href="attendance_check.php">Check Attendance</a></li>
-                    <li><a href="check_hours.php">Check Hours</a></li>
-                    <li><a href="edit_exec.php">Edit Exec</a></li>
-                    <li><a href="member_statuses.php">Edit Members</a></li>
-                </ul>
-        </li>
+		<li><a href="schedule.php">Conference Schedule</a></li>
+		<li><a href="register.php">Register</a></li>
+		<li><a href="login.php">Login</a></li>
 	</ul>
     <!--<div id="righttriangle"></div>-->
