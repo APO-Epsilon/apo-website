f0675653699c2790f2e4bd94fa8f8c11fe4f3a08
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-11 14:58:08 -0600

    Update footer.php

--- r8conf2016/footer.php
+++ r8conf2016/footer.php
@@ -2,7 +2,7 @@
         <ul class="icons">
                     <li><a href="//www.twitter.com/apo_epsilon" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
                     <li><a href="//www.facebook.com/apo.epsilon" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
-                    <li><a href="https://github.com/racedale/apo-website" class="icon circle fa-github"><span class="label">Github</span></a></li>
+                    <li><a href="https://github.com/APO-Epsilon/apo-website" class="icon circle fa-github"><span class="label">Github</span></a></li>
                 </ul>
 
         <p>2015 &copy; Alpha Phi Omega - Epsilon Chapter
