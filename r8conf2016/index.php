82884f73c6a806ad88f5c3c5a264ec842b9ab014
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-10 19:25:20 -0600

    Update index.php

--- r8conf2016/index.php
+++ r8conf2016/index.php
@@ -25,6 +25,10 @@ require_once ('session.php');
             <p>Nice little blurb about the conference here</p>
             <br><br>
         </div>
+        <div class="large-4 medium-4 small-12 columns">
+            <a class="twitter-timeline" width="250" height="600" href="https://twitter.com/APORegionals16" data-widget-id="565319444856328196">Tweets by @APORegionals16</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
         <div class="large-2 medium-2 small-6 columns">
             <a href="register.php" class="button expand">Register</a>
         </div>
