5f3f201a43a26f63c863053b83313c07889ab8b5
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-10 20:34:20 -0600

    Update status_change.php

--- status_change.php
+++ status_change.php
@@ -131,41 +131,6 @@ echo<<<END
 						<input type="text" name="first_name" value="$row[firstname]" placeholder="First name" required="" autocomplete="name"/>
 					<label for="last_name">Last Name</label>
 						<input type="text" name="last_name" value="$row[lastname]" placeholder="Last name" required="" autocomplete="name"/>
-					<select name="bday" id="bday">
-					    <option>$row[bday]</option>
-						<option>01</option>
-						<option>02</option>
-						<option>03</option>
-						<option>04</option>
-						<option>05</option>
-						<option>06</option>
-						<option>07</option>
-						<option>08</option>
-						<option>09</option>
-						<option>10</option>
-						<option>11</option>
-						<option>12</option>
-						<option>13</option>
-						<option>14</option>
-						<option>15</option>
-						<option>16</option>
-						<option>17</option>
-						<option>18</option>
-						<option>19</option>
-						<option>20</option>
-						<option>21</option>
-						<option>22</option>
-						<option>23</option>
-						<option>24</option>
-						<option>25</option>
-						<option>26</option>
-						<option>27</option>
-						<option>28</option>
-						<option>29</option>
-						<option>30</option>
-						<option>31</option>
-					</select>
-					<input name="byear" type="text" style="width: 50px;" maxlength="4" value="$row[byear]"/>
 				<br>
 				<b>Contact</b><br>
 					<label for="email">Email</label>
