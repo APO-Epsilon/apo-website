10a4890092d6a6fd08375120e8384e6605a5e2ef
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-14 00:54:57 -0600

    Update register_process.php

--- r8conf2016/register_process.php
+++ r8conf2016/register_process.php
@@ -63,7 +63,7 @@ $mail->Password = "alphaphiomega";
 $mail->FromName = "Alpha Phi Omega Epsilon";
 $mail->AddAddress("apo.epsilon.conferencechair@gmail.com", "APO Epsilon Conference Chair");
 $resultcount = mysqli_fetch_array($db->query("SELECT COUNT(*) FROM conf_contact_information;"));
-$subject = "(" . $resultcount . ") " . $firstname . " " . $lastname . " has registered for the APO Epsilon Region VIII Conference";
+$subject = "(" . $resultcount['COUNT(*)'] . ") " . $firstname . " " . $lastname . " has registered for the APO Epsilon Region VIII Conference";
 $emailToSend = "Name: " . $firstname . " " . $lastname . "\n" . 
     "Additional Information submitted will go here.";
 
