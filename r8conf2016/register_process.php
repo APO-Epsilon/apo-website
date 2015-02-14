96d19e9584af55bfcf3af6ad9e1812f6affc2fdf
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-13 23:45:41 -0600

    Update register_process.php

--- r8conf2016/register_process.php
+++ r8conf2016/register_process.php
@@ -50,6 +50,7 @@ $result = $db->query($insert);
 if (!$result) {
     die('There has an been error with your registration. This may be because the email address you supplied is already in use, or there may be other technical problems. If the error persists, please contact the webmaster at <a href="mailto:apo.epsilon.webmaster@gmail.com">apo.epsilon.webmaster@gmail.com.</a>' . mysqli_error());
 }
+echo "0";
 require("./phpmailer/class.phpmailer.php");
 $mail = new PHPMailer();
 $mail->IsSMTP();                                      // set mailer to use SMTP
