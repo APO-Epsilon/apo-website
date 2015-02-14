78649d91bead9edc7e0c3896aef5dd007cac61ae
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-13 21:32:01 -0600

    Update login_forgotpw.php

--- r8conf2016/login_forgotpw.php
+++ r8conf2016/login_forgotpw.php
@@ -18,14 +18,14 @@ require_once ('mysql_access.php');
     <!-- PHP method to include header -->
 
     <div class="row">
-        <div class="medium-8 small-12 medium-centered columns">
+        <div class="medium-8 small-12 columns">
             <p>Please enter the email address you used when you signed up, and we will send your password to you immediately. <br><br>
             If you don't receive an email, that means you either typed the email wrong, or registered with a different email address.  If you need help, please contact the webmaster at <a href="mailto:apo.epsilon.webmaster@gmail.com">apo.epsilon.webmaster@gmail.com</a>.
             </p>
         </div>
 
         <form method="GET" action="login_sendpw.php" >
-            <div class="small-12 columns">
+            <div class="medium-6 small-12 columns">
                 <label>E-mail</label><input type="text" size="30" name="email" id="email" value="" />
         	</div>
         	<div class="medium-3 small-12 medium-centered columns">
