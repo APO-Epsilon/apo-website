49e14a4e151d55e5ce5e4f36276ae415636ada09
als44 <als4462@truman.edu>
2015-02-16 14:34:38 -0600

    Update service_sign-up.php

--- service_sign-up.php
+++ service_sign-up.php
@@ -1,24 +1,21 @@
 <?php
-include ('session.php');
-include ('mysql_access.php');
 require_once ('session.php');
 ?>
 <!doctype html>
 <html>
 <head>
-  <? head require 'head.php'; ?>
+    <?php require 'head.php';?>
 </head>
+
 <body class="slide" data-type="background" data-speed="5">
-<!-- Javascript method to include navigation -->
-<nav id="nav" role="navigation"><?php include 'nav.php';?></nav>
-<!-- PHP method to include navigation -->
-<!-- Javascript method to include header -->
-<div id="header"><?php include 'header.php';?></div>
-<!-- PHP method to include header -->
-
-<p>
-future service on the website.
-</p>
-
-<div id="footer"><?php include 'footer.php';?></div>
+    <nav id="nav" role="navigation"></nav>
+    <div id="header"></div>
+    <div class="row">
+        <div class="large-6 medium-9 small-12 columns">
+           future service sign up!
+    </div>
+    <!-- Javascript method to include footer -->
+    <div id="footer"><?php include 'footer.php';?></div>
+    <!-- PHP method to include footer -->
+</body>
 </html>
