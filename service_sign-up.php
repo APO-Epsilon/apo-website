1b70b150c2043da4a3eb261c308c55d03815efee
als44 <als4462@truman.edu>
2015-02-16 14:25:07 -0600

    updated service_sign-up

---
+++ service_sign-up.php
@@ -0,0 +1,13 @@
+<?php
+include ('session.php');
+include ('mysql_access.php');
+?>
+<!doctype html>
+<html>
+<head>
+  <? head require 'head.php'; ?>
+</head>
+<p>
+future service on the website.
+</p>
+</html>
