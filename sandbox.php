54ac1d3edca7e1c8700c4103aea276385748d94a
als44 <als4462@truman.edu>
2015-02-16 13:43:44 -0600

    creativity welcome

---
+++ sandbox.php
@@ -0,0 +1,22 @@
+<?php
+require_once ('session.php');
+?>
+<!doctype html>
+<html>
+<head>
+    <?php require 'head.php';?>
+</head>
+
+<body class="slide" data-type="background" data-speed="5">
+    <nav id="nav" role="navigation"></nav>
+    <div id="header"></div>
+    <div class="row">
+        <p>
+        Hello world!
+        </p>
+    </div>
+    <!-- Javascript method to include footer -->
+    <div id="footer"><?php include 'footer.php';?></div>
+    <!-- PHP method to include footer -->
+</body>
+</html>
