17f674b527d5d25953398f896177f678bf82a4ee
als44 <als4462@truman.edu>
2015-02-16 14:31:47 -0600

    Update service_sign-up.php

--- service_sign-up.php
+++ service_sign-up.php
@@ -8,6 +8,14 @@ require_once ('session.php');
 <head>
   <? head require 'head.php'; ?>
 </head>
+<body class="slide" data-type="background" data-speed="5">
+<!-- Javascript method to include navigation -->
+<nav id="nav" role="navigation"><?php include 'nav.php';?></nav>
+<!-- PHP method to include navigation -->
+<!-- Javascript method to include header -->
+<div id="header"><?php include 'header.php';?></div>
+<!-- PHP method to include header -->
+
 <p>
 future service on the website.
 </p>
