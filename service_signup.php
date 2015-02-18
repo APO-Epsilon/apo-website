c9ca65e5a5ef9859499efb4c4dd6e12f9c929568
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-17 21:49:54 -0600

    Update service_signup.php

--- service_signup.php
+++ service_signup.php
@@ -98,6 +98,11 @@ $sql = "SELECT d.detail_id, d.event_id, d.DOW,
 		WHERE o.active = 1 
 		ORDER BY o.theDate, o.start";
 $resultO = $db->query($sql);
+if($db){
+	echo "yes";
+}else
+	echo "no";
+}
 	if(!$resultO){
 		die("error 0");
 	}else{
