68ec0c16728d32afe3c7ec7b46c1c7f9783d63c1
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-17 22:06:07 -0600

    Update service_signup.php

--- service_signup.php
+++ service_signup.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['sessionID'])) {
 }else{
 
 function refresh(){
-	echo("<meta http-equiv=\"REFRESH\" content=\"0;url=http://apoepsilon.org/service_signup.php\">");
+	echo("<meta http-equiv=\"REFRESH\" content=\"0;url=http://www.apoepsilon.org/service_signup.php\">");
 }
 
 
@@ -134,12 +134,12 @@ $resultO = $db->query($sql);
 			if(($count < $max)||($num_rows == 1)||($max==-1)){
 				if($num_rows == 0){
 					$m = 1;
-					$message = "<a href=\"http://apoepsilon.org/service_signup.php?d=$detail_id&o=$occurrence_id\">sign-up</a>";
+					$message = "<a href=\"http://www.apoepsilon.org/service_signup.php?d=$detail_id&o=$occurrence_id\">sign-up</a>";
 					//$message = "";
 				}else{
 					$m = 2;
-					$message = "<a href=\"http://apoepsilon.org/service_signup.php?r=$detail_id&o=$occurrence_id\">remove</a>";	
-					$message;
+					$message = "<a href=\"http://www.apoepsilon.org/service_signup.php?r=$detail_id&o=$occurrence_id\">remove</a>";	
+					//$message;
 				}
 			}else{
 				$message = "full";
