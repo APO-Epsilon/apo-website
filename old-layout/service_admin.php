745b7073c67e421972c1f78b0a8cb9a70f2452ef
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-19 17:12:21 -0600

    Update service_admin.php

--- old-layout/service_admin.php
+++ old-layout/service_admin.php
@@ -8,8 +8,6 @@ $id = $_SESSION['sessionID'];
 $position = $_SESSION['sessionposition'];
 echo "<div class=\"content\">";
 if($position != "Webmaster" && $position != "VP of Regular Service"){
-	echo"test";
-	echo "$position";
 	die("you do not have permission to view this page.");
 }
 echo "<h1>Service Manager: VP of Regular Service</h1><hr/>";
