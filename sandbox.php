ea00b6d1948aa04fdd37b8836de4b8bb31996c85
Nicholas Greer Eaton <nge4445@truman.edu>
2015-02-19 19:27:10 -0800

    Update sandbox.php

--- sandbox.php
+++ sandbox.php
@@ -19,7 +19,7 @@ require_once ('session.php');
 var snowmax=35
 
 // Set the colors for the snow. Add as many colors as you like
-var snowcolor=new Array("#aaaacc","#ddddFF","#ccccDD")
+var snowcolor=new Array("#aaaacc","#ddddFF","#ccccDD"," #7A007A")
 
 // Set the fonts, that create the snowflakes. Add as many fonts as you like
 var snowtype=new Array("Arial Black","Arial Narrow","Times","Comic Sans MS")
