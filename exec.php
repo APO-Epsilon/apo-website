5582162a5643403253739296c7d04bb4689c713a
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-02-27 20:25:57 -0600

    Updated Exec page
    Now includes photos. Uses get_photo.

--- exec.php
+++ exec.php
@@ -15,24 +15,28 @@ require_once ('mysql_access.php');
     <ul class="large-block-grid-2 small-block-grid-1">
 <?php
 include('mysql_access.php');
+include('get_photo.php');
 $sql = "SELECT position, position_email FROM positions WHERE position_order <> 0 ORDER BY position_order ASC;";
 $result = $db->query($sql);
 while($position_query = mysqli_fetch_array($result)){
 	$current_position = $position_query['position'];
-	$sql2 = "SELECT firstname, lastname FROM contact_information WHERE position='$current_position';";
+	$sql2 = "SELECT id, firstname, lastname FROM contact_information WHERE position='$current_position';";
 	$result2 = $db->query($sql2);
 	if(mysqli_num_rows($result2) == 0){
 		;
 	}else{
-		echo "<li><div class=\"small-12 columns\"><h1>$current_position</h1><p>";
+		echo "<li><div class=\"row\"><div class=\"small-12 columns\"><h1>$current_position</h1></div>";
 		while($name_query = mysqli_fetch_array($result2)){
+			echo "<div class=\"row\"><div class=\"small-2 columns\">";
+			echo "<img src=\"" . getPhotoLink($name_query['id']) . "\">";
+			echo "</div>";
 			$firstname = $name_query['firstname'];
 			$lastname = $name_query['lastname'];
-			echo $firstname . " " . $lastname . "<br>";
+			echo "<div class=\"small-10 columns\"><div class=\"row\"><div class=\"small-12 columns\"><p> </p></div><div class=\"small-12 columns\"><p>" . $firstname . " " . $lastname . "</p></div></div></div></div>";
 		}
 		$position_email = $position_query['position_email'];
-		echo ("<a href=\"mailto:" . $position_email . "\">" . $position_email . "</a>");
-		echo "</p><hr></div></li>";
+		echo ("<div class=\"row\"><div class=\"small-10 small-offset-2 columns\"><p><a href=\"mailto:" . $position_email . "\">" . $position_email . "</a></p></div></div>");
+		echo "</div></li>";
 	}
 }
 ?>
