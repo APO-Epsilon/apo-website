c64076e3c8e8eca994352613dc4d83cde1e49b8f
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-03-02 00:03:35 -0600

    Added photos/pledge class toggle to actives page
    Kevin contributed the toggle code

--- actives.php
+++ actives.php
@@ -1,120 +1,46 @@
 <?php
 require_once('session.php');
 require_once('mysql_access.php');
+require_once('get_photo.php');
 ?>
 <!doctype html>
 <html>
 <head>
     <?php require 'head.php';?>
+    <script src="/js/vendor/jquery.js"></script>
 </head>
 
 <body class="slide" data-type="background" data-speed="5">
     <nav id="nav" role="navigation"></nav>
     <div id="header"></div>
     <div class="row">
-        <div class="large-6 medium-9 small-12 small-centered columns text-center">
-            <h1> Fall 2011</h1>
-                <p>  <br>
-                <a href="#" ></a>
-                </p>
-		<?php
-			include('mysql_access.php');
-			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Fall' AND pledgeyear='2011' ORDER BY lastname ASC, firstname ASC";
-			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
-			while ($t = mysqli_fetch_array($querym)){
-				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
-			}
-		?>
-            <hr>
-            <h1> Spring 2012 </h1>
-                <p>  <br>
-                <a href="#"></a>
-                </p>
         <?php
-			include('mysql_access.php');
-			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Spring' AND pledgeyear='2012' ORDER BY lastname ASC, firstname ASC"; 			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
- 			while ($t = mysqli_fetch_array($querym)){
- 				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
- 			}
-		?>
-            <hr>
-            <h1> Fall 2012 </h1>
-                <p> <br>
-                <a href="#"></a>
-                </p>
-        <?php
- 			include('mysql_access.php');
- 			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Fall' AND pledgeyear='2012' ORDER BY lastname ASC, firstname ASC";
- 			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
- 			while ($t = mysqli_fetch_array($querym)){
- 				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
- 			}
-		?>
-            <hr>
-            <h1> Spring 2013 </h1>
-                <p>  <br>
-                <a href="#"></a>
-                </p>
-        <?php
- 			include('mysql_access.php');
- 			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Spring' AND pledgeyear='2013' ORDER BY lastname ASC, firstname ASC";
- 			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
- 			while ($t = mysqli_fetch_array($querym)){
- 				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
- 			}
-		?>
-            <hr>
-            <h1> Fall 2013 </h1>
-                <p> <br>
-                <a href="#"></a>
-                </p>
-        <?php
- 			include('mysql_access.php');
- 			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Fall' AND pledgeyear='2013' ORDER BY lastname ASC, firstname ASC";
- 			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
- 			while ($t = mysqli_fetch_array($querym)){
- 				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
- 			}
-		?>
-            <hr>
-            <h1> Spring 2014 </h1>
-                <p> <br>
-                <a href="#"></a>
-                </p>
-        <?php
- 			include('mysql_access.php');
- 			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Spring' AND pledgeyear='2014' ORDER BY lastname ASC, firstname ASC";
- 			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
- 			while ($t = mysqli_fetch_array($querym)){
- 				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
- 			}
-		?>
-            <hr>
-            <h1> Fall 2014 </h1>
-                <p>  <br>
-                <a href="#"></a>
-                </p>
-        <?php
- 			include('mysql_access.php');
- 			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Fall' AND pledgeyear='2014' ORDER BY lastname ASC, firstname ASC";
- 			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
- 			while ($t = mysqli_fetch_array($querym)){
- 				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
- 			}
-		?>
-            <hr>
-            <h1> Spring 2015 </h1>
-                <p>  <br>
-                <a href="#"></a>
-                </p>
-       <?php
- 			include('mysql_access.php');
- 			$selectm = "SELECT firstname, lastname, major, minor, status, position, pledgesem, pledgeyear FROM contact_information WHERE pledgesem='Spring' AND pledgeyear='2015' ORDER BY lastname ASC, firstname ASC";
- 			$querym = $db->query($selectm) or die("If you encounter problems, please contact the webmaster.");
- 			while ($t = mysqli_fetch_array($querym)){
- 				echo "<p>" . $t['firstname'] . " " . $t['lastname'] . "</p>";
- 			}
-		?>
-        </div>
+        	$sql = "SELECT DISTINCT pledgeyear FROM contact_information ORDER BY pledgeyear ASC;";
+        	$yearResult = $db->query($sql);
+        	while($yearArray = mysqli_fetch_array($yearResult)){
+        		$year = $yearArray['pledgeyear'];
+        		$sql = "SELECT DISTINCT pledgesem FROM contact_information WHERE pledgeyear=\"$year\" ORDER BY pledgesem DESC;";
+        		$semResult = $db->query($sql);
+        		while($semArray = mysqli_fetch_array($semResult)){
+        			$sem = $semArray['pledgesem'];
+        			echo "<div class=\"small-12 columns small-centered text-center\"><button id=\"{$sem}{$year}button\">$sem $year</button></div>";
+        			echo "<ul id=\"{$sem}{$year}\" class=\"large-block-grid-6 medium-block-grid-4 small-block-grid-2\">";
+        			$sql = "SELECT id, firstname, lastname FROM contact_information WHERE pledgeyear=\"$year\" AND pledgesem=\"$sem\" AND status<>\"Alumni\" ORDER BY lastname ASC;";
+        			$activeResult = $db->query($sql);
+        			while($active = mysqli_fetch_array($activeResult)){
+        				$id = $active['id'];
+        				$firstname = $active['firstname'];
+        				$lastname = $active['lastname'];
+        				$activePhotoLink = getPhotoLink($id);
+        				echo "<li><div class=\"small-12 small-centered columns text-center\"><img src=\"{$activePhotoLink}\" width=\"75\" height=\"100\"><br><p>$firstname $lastname</p></div></li>\n";
+        			}
+        			echo "</ul>";
+        			echo "<script type=\"text/javascript\">\$(document).ready(function(){\$(\"#{$sem}{$year}button\").click(function(){\$(\"#{$sem}{$year}\").toggle();});";
+        			echo "\$(\"#{$sem}{$year}\").toggle();});</script>";
+        		}
+        	}
+        ?>
+    </div>
+    <div id="footer"><?php include 'footer.php';?></div>
 </body>
 </html>
