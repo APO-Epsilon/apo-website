656f9f85fab146cb58a43de7b1a546a15b502d92
Nicholas Greer Eaton <nge4445@truman.edu>
2015-03-05 13:57:43 -0800

    Update snakes.php

--- snakes.php
+++ snakes.php
@@ -1,6 +1,1 @@
-<!-- Place this code where you'd like the game to appear -->
-<div class="miniclip-game-embed" data-game-name="snake" data-theme="0" data-width="450" data-height="360" data-language="en"><a href="http://www.miniclip.com/games/snake/">Play Snake</a></div>
-<p style="text-align:center;"><a href="http://www.miniclip.com/games/snake/" target="_blank">Play Snake</a> / More <a href="http://www.miniclip.com/games/genre-1/" target="_blank">Puzzle games</a></p>
 
-<!-- Insert this code before your </body> tag -->
-<script src="//static.miniclipcdn.com/js/game-embed.js"></script>
