6cb6ad8d133fb6175a2e1872011e236a6667e0b1
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-03-23 03:43:19 -0500

    family_tree update
    Layout update and script click events added

--- family_tree.php
+++ family_tree.php
@@ -13,6 +13,10 @@ require_once ('session.php');
 
 <div class="row">
 	<div class="small-12 columns">
+		<h2>APO Epsilon Family Tree</h2><br>
+		<div id='family_tree' style='overflow: auto; height: 90%;'></div>
+	</div>
+
 
 <?php
 $exec_page = False;
@@ -22,10 +26,9 @@ require_once('permissions.php');
 
 function show_public() {
 	include('mysql_access.php');
-	echo "<h2>APO Epsilon Family Tree</h2><br>";
-	echo "<div id='family_tree' style='overflow: auto; height: 90%;'></div>";
-	echo "<script type='text/vnd.graphviz' id='family_tree_script'>";
+	//generate dot for viz.js
 	echo <<<END
+	<script type='text/vnd.graphviz' id='family_tree_script'>
 	digraph "family_tree" {
 	graph [ bgcolor = transparent ];
 	node [	shape = polygon,
@@ -40,7 +43,7 @@ function show_public() {
 END;
 	$colorarray = array("maroon", "red", "green", "chartreuse", "plum", "salmon", "goldenrod", "yellow", "blue", "cyan");
 	$rank = "";
-	$sql = "SELECT DISTINCT pledgeyear, pledgesem FROM contact_information ORDER BY pledgeyear ASC, pledgesem DESC;";
+	$sql = "SELECT DISTINCT pledgeyear, pledgesem FROM contact_information WHERE pledgeyear<>\"\" AND pledgesem<>\"\" ORDER BY pledgeyear ASC, pledgesem DESC;";
     $result = $db->query($sql);
     while ($row = mysqli_fetch_array($result)) {
     	$pledgeyear = $row['pledgeyear'];
@@ -56,7 +59,9 @@ END;
 			$id = $row2['id'];
 			$firstname = $row2['firstname'];
 			$lastname = $row2['lastname'];
+			//make nodes
 			echo "\"$id\" [label=\"$firstname $lastname\", id=\"$id\", color=$color];\n";
+			//generate rank for each pledge class
 			$rank .= " " . $id;
 		}
 		$rank .= ";}\n";
@@ -72,6 +77,8 @@ END;
 	echo "} </script>";
 	echo "<script src='/js/viz.js/viz.js'></script>";
 	echo "<script>document.getElementById('family_tree').innerHTML = Viz(document.getElementById('family_tree_script').innerHTML, \"svg\", \"dot\")</script>";
+	//generate dropdown box
+	echo "<div class=\"medium-6 small-12 columns\">";
 	echo "<select id=\"memberselect\">\n";
 	$sql = "SELECT id, firstname, lastname FROM contact_information ORDER BY lastname ASC;";
 	$result = $db->query($sql);
@@ -79,22 +86,41 @@ END;
     	echo "<option value=\"{$row['id']}\">{$row['firstname']} {$row['lastname']}</option>\n";
     }
 	echo "</select>";
-	echo "<div id=\"member_info\"></div>";
+	echo "</div>";
+	echo "<div class=\"medium-6 small-12 columns\"><div id=\"member_info\"></div></div>";
 	echo <<<END
 	<script>
 		//jQuery is currently included on every page by the site. If this changes, include the src to it above this script
-		$("#memberselect").change(function() {
-			var elementid = $("#" + $(this).val());
+		function loadmemberinfo(id) {
+			$("#member_info").load("includes/family_tree_info.php?user_id=" + id);
+		}
+
+		function showmemberontree(id) {
+			var elementid = $("#" + id);
 			var container = $("#family_tree");
 			$("#family_tree").animate({
 				scrollLeft: elementid.offset().left + elementid.get(0).getBBox().width/2 - container.offset().left + container.scrollLeft() - container.width()/2,
 				scrollTop: elementid.offset().top + elementid.get(0).getBBox().height/2 - container.offset().top + container.scrollTop() - container.height()/2
 			}, 600);
+		}
+
+		$("#memberselect").change(function() {
+			var id = $(this).val();
+			showmemberontree(id);
+			loadmemberinfo(id);
 		});
-		$("g").click(function(event) {
-			console.log(this.id);
+
+		$("#family_tree").on("click", "g", function(event) {
 			event.stopPropagation();
-			$("#member_info").load("includes/member_info.php?user_id=" + this.id);
+			showmemberontree(this.id);
+			loadmemberinfo(this.id);
+			$("html, body").animate({scrollTop: $("#member_info").offset().top}, 600);
+		});
+
+		$("#member_info").on("click", ".biglittle", function() {
+			var id = $(this).attr("id").substr(2);
+			showmemberontree(id);
+			loadmemberinfo(id);
 		});
 	</script>
 END;
