786e4e4f5e7931e85f6dd7fce15f979f115116f8
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-03-23 03:40:42 -0500

    Bugfix get_photo
    get_photo now points link to user_photo. Avoids bug where GET user_id
    already set causes raw picture data to be dumped to page where get_photo
    is included

--- get_photo.php
+++ get_photo.php
@@ -9,21 +9,8 @@ function getPhotoLink($user_id){
 			if(mysqli_num_rows($result) == 0){
 				return "img/unknown.jpg";
 			}else{
-				return basename(__FILE__) . "?user_id=" . $user_id;
+				return "/user_photo.php?user_id=" . $user_id;
 			}
-}
-
-if(isset($_GET['user_id']) && is_numeric($_GET['user_id'])){
-	include ('mysql_access.php');
-	$sql = "SELECT content FROM user_photos WHERE user_id=" . $_GET['user_id'] . ";";
-	$result = $db->query($sql);
-	if(!$result || mysqli_num_rows($result) == 0){
-		;
-	}else{
-		$image_array = mysqli_fetch_array($result);
-		header("Content-type: image/jpeg");
-		echo $image_array['content'];
-	}
 }
 
 ?>
