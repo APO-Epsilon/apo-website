8d885403667333242c62bdaf1dc068ad51ae972e
Dale <race.dale@gmail.com>
2015-04-24 17:39:31 -0500

    Upload an angular version to /ng as a test

---
+++ ng/api/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Database configuration
+ */
+/*
+$DBServer = 'apo.cxav86kuligx.us-west-2.rds.amazonaws.com';
+$DBUser   = 'apo';
+$DBPass   = 'alphaphiomega';
+$DBName   = 'apo';
+*/
+define('DB_HOST', 'apo.cxav86kuligx.us-west-2.rds.amazonaws.com');
+define('DB_NAME', 'apo');
+define('DB_USER','apo');
+define('DB_PASSWORD','alphaphiomega');
+
+
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'angularcode');
+
+?>
---
+++ ng/app/routes.js
@@ -0,0 +1,1 @@
+var foundationRoutes = [{"name":"settings","url":"/settings-this-is-cool","animationIn":"slideFromLeft","animationOut":"slideFromLeft","path":"templates/settings.html"},{"name":"home","url":"/","path":"templates/home.html"}];
