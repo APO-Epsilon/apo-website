6759e6744bf09ea794901b2a0eaa460d88f8d65b
Dale <race.dale@gmail.com>
2015-04-24 20:40:47 -0500

    Removed from foundation-apps

--- ng/app/routes.js
+++
@@ -1,1 +0,0 @@
-var foundationRoutes = [{"name":"settings","url":"/settings-this-is-cool","animationIn":"slideFromLeft","animationOut":"slideFromLeft","path":"templates/settings.html"},{"name":"home","url":"/","path":"templates/home.html"}];
