8b56e78d0c88ea046cd48776ff6a20f2a170699f
Dale <race.dale@gmail.com>
2015-04-27 19:20:17 -0500

    Reorder comments so that it's back to localhost

--- ng/api/config.php
+++ ng/api/config.php
@@ -8,15 +8,15 @@
 // $DBPass   = 'alphaphiomega';
 // $DBName   = 'apo';
 
-define('DB_HOST', 'apo.cxav86kuligx.us-west-2.rds.amazonaws.com');
-define('DB_NAME', 'apo');
-define('DB_USER','apo');
-define('DB_PASSWORD','alphaphiomega');
+// define('DB_HOST', 'apo.cxav86kuligx.us-west-2.rds.amazonaws.com');
+// define('DB_NAME', 'apo');
+// define('DB_USER','apo');
+// define('DB_PASSWORD','alphaphiomega');
 
 
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_HOST', 'localhost');
-// define('DB_NAME', 'angularcode');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'angularcode');
 
 ?>
