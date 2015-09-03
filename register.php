687c51400cad938e160830161ebaa76383bac68a
ahoopes16 <kah4627@truman.edu>
2015-09-03 18:25:24 -0500

    Update register.php

--- register.php
+++ register.php
@@ -86,6 +86,7 @@ require_once ('mysql_access.php');
 					</select>
 					<select name="pledgeyear">
 						<option value="2015">2015</option>
+						<option value="2014">2014</option>
 						<option value="2013">2013</option>
 						<option value="2012">2012</option>
 						<option value="2011">2011</option>
