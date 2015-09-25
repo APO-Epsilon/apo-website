9aa44591e37bccafa689804c5e1612f255fa8268
Moon "Moonman" Man <nge4445@truman.edu>
2015-09-24 17:17:17 -0700

    Update Conference _Exec.php

--- r8conf2016/Conference _Exec.php
+++ r8conf2016/Conference _Exec.php
@@ -1,5 +1,7 @@
 <?php
 require_once ('session.php');
+require_once ('mysql_access.php');
+require_once ('get_photo.php');
 ?>
 <!doctype html>
 <html>
@@ -8,22 +10,36 @@ require_once ('session.php');
 </head>
 
 <body class="slide" data-type="background" data-speed="5">
-    <!-- Javascript method to include navigation -->
-    <nav id="nav" role="navigation"><?php include 'nav.php';?></nav>
-    <!-- PHP method to include navigation -->
-
-    <!-- Javascript method to include header -->
-    <div id="header"><?php include 'header.php';?></div>
-    <!-- PHP method to include header -->
-
-
-<div class="row">
-    <div class="large-8 medium-8 small-12 columns">
-        <h2>Conference Schedule</h2>
-    </div>
+    <nav id="nav" role="navigation"></nav>
+    <div id="header"></div>
+    <div class="row">
+    <ul class="large-block-grid-2 small-block-grid-1">
+<?php
+$sql = "SELECT position, position_email FROM positions WHERE position_order <> 0 ORDER BY position_order ASC;";
+$result = $db->query($sql);
+while($position_query = mysqli_fetch_array($result)){
+	$current_position = $position_query['position'];
+	$sql2 = "SELECT id, firstname, lastname FROM contact_information WHERE position='$current_position';";
+	$result2 = $db->query($sql2);
+	if(mysqli_num_rows($result2) == 0){
+		;
+	}else{
+		echo "<li><div class=\"row\"><div class=\"small-12 columns\"><h1 class=\"text-center\">$current_position</h1></div><ul class=\"small-block-grid-3 text-center\">";
+		while($name_query = mysqli_fetch_array($result2)){
+			echo "<li style=\"float: none; display: inline-block;\"><div class=\"small-12 columns\">";
+			echo "<img src=\"" . getPhotoLink($name_query['id']) . "\" width=\"125\" style=\"display: block; margin-left: auto; margin-right: auto;\">";
+			$firstname = $name_query['firstname'];
+			$lastname = $name_query['lastname'];
+			echo "<h4 class=\"text-center\">" . $firstname . "<br>" . $lastname . "</h4></div></li>";
+		}
+		$position_email = $position_query['position_email'];
+		echo ("</ul><div class=\"small-12 columns\"><p class=\"text-center\"><a href=\"mailto:" . $position_email . "\">" . $position_email . "</a></p></div></div>");
+		echo "</li>";
+	}
+}
+?>
+</ul>
 </div>
-
-
     <!-- Javascript method to include footer -->
     <div id="footer"><?php include 'footer.php';?></div>
     <!-- PHP method to include footer -->
