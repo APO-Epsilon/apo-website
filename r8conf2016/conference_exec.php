f71bb7c5ad9ca99451456475a9c8bf6e980f6c88
Dale <race.dale@gmail.com>
2015-09-25 18:51:33 -0500

    Removed extra title

--- r8conf2016/conference_exec.php
+++ r8conf2016/conference_exec.php
@@ -15,12 +15,12 @@ require_once ('../get_photo.php');
     <div class="row">
     <ul class="large-block-grid-2 small-block-grid-1">
 
-      <!-- This is the part that gets repeated for each person -->
       <div class="row">
         <div class="small-12 columns">
           <h1 class="text-center">Conference Executive Member</h1>
         </div>
 
+      <!-- This is the part that gets repeated for each person -->
         <ul class="small-block-grid-3 text-center">
       		<li style="float: none; display: inline-block;">
             <div class="small-12 columns">
@@ -42,12 +42,8 @@ require_once ('../get_photo.php');
         </div>
       </div>
       <!-- This is the end of the part that gets repeated for each person -->
-            <!-- This is the part that gets repeated for each person -->
-      <div class="row">
-        <div class="small-12 columns">
-          <h1 class="text-center">Conference Executive Member</h1>
-        </div>
 
+      <!-- This is the part that gets repeated for each person -->
         <ul class="small-block-grid-3 text-center">
           <li style="float: none; display: inline-block;">
             <div class="small-12 columns">
@@ -69,8 +65,9 @@ require_once ('../get_photo.php');
         </div>
       </div>
       <!-- This is the end of the part that gets repeated for each person -->
-</ul>
-</div>
+
+  </ul>
+  </div>
     <!-- Javascript method to include footer -->
     <div id="footer"><?php include 'footer.php';?></div>
     <!-- PHP method to include footer -->
