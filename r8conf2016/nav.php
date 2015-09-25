d4353eeb39a2449d4589f85ded0e6151606fe1bf
Dale <race.dale@gmail.com>
2015-09-25 18:32:36 -0500

    un-capitalize

--- r8conf2016/nav.php
+++ r8conf2016/nav.php
@@ -6,8 +6,8 @@
 		<li><a href="schedule.php">Schedule</a></li>
 		<li><a href="register.php">Register</a></li>
 		<li><a href="jaredbox.php">The Jared Box</a></li>
-		<li><a href="Conference_Exec.php">Conference Exec</a></li>
-	
+		<li><a href="conference_exec.php">Conference Exec</a></li>
+
 		<!--Last list item will be name if logged in>
 	</ul>
     <!--<div id="righttriangle"></div>-->
