da3adee962299325d77b515515fbf9cf7c00a2e8
ahoopes16 <kah4627@truman.edu>
2015-09-24 19:14:14 -0500

    Create jaredbox.php

---
+++ r8conf2016/jaredbox.php
@@ -0,0 +1,24 @@
+<?php
+require_once ('session.php');
+require_once ('../mysql_access.php');
+?>
+<!doctype html>
+<html>
+<head>
+    <?php require 'head.php';?>
+</head>
+
+<body class="slide" data-type="background" data-speed="5">
+    <!-- Javascript method to include navigation -->
+    <nav id="nav" role="navigation"><?php include 'nav.php';?></nav>
+    <!-- PHP method to include navigation -->
+
+    <!-- Javascript method to include header -->
+    <div id="header"><?php include 'header.php';?></div>
+    <!-- PHP method to include header -->
+
+	<div class="row">
+    
+  <div class="small-12 columns">
+			 <h2>The Jared Box</h2>
+			    
