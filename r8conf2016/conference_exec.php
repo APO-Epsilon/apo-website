d4b77db1da75171ad266f4ddc29b2b79615defaf
Dale <race.dale@gmail.com>
2015-09-25 19:21:35 -0500

    Added more people

--- r8conf2016/conference_exec.php
+++ r8conf2016/conference_exec.php
@@ -23,12 +23,13 @@ require_once ('../get_photo.php');
         <div class="small-12 medium-6 large-4 columns">
       		<img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
       		<h4 class="text-center">
-            Name
+            Conference Chair
             <br>
           </h4>
           <p class="text-center">
-            <a href="mailto:">
-              Email
+            Joe Hall
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
             </a>
           </p>
         </div>
@@ -38,12 +39,134 @@ require_once ('../get_photo.php');
         <div class="small-12 medium-6 large-4 columns">
           <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
           <h4 class="text-center">
-            Second Name
+            Conference Advisor
             <br>
           </h4>
           <p class="text-center">
-            <a href="mailto:">
-              Email
+            David Gilbert
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+            <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Logistics Chair
+            <br>
+          </h4>
+          <p class="text-center">
+            Ashley Banze
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+
+        <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Public Relations
+            <br>
+          </h4>
+          <p class="text-center">
+            Emily Leddin
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+      <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Treasurer
+            <br>
+          </h4>
+          <p class="text-center">
+            Josh Bright
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+      <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Workshops
+            <br>
+          </h4>
+          <p class="text-center">
+            Valerie Frerichs
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+      <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Brotherhood
+            <br>
+          </h4>
+          <p class="text-center">
+            Trista Sullivan
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+      <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Bidding Committee Liason
+            <br>
+          </h4>
+          <p class="text-center">
+            Libby Moser
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+      <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Brother Housing
+            <br>
+          </h4>
+          <p class="text-center">
+            Molly Turner and Rick Cazzato Jr.
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
+            </a>
+          </p>
+        </div>
+      <!-- This is the end of the part that gets repeated for each person -->
+      <!-- This is the part that gets repeated for each person -->
+        <div class="small-12 medium-6 large-4 columns">
+          <img src="../img/unknown.jpg" width="125" style="display: block; margin-left: auto; margin-right: auto;">
+          <h4 class="text-center">
+            Ceremony
+            <br>
+          </h4>
+          <p class="text-center">
+            Taylor Demien
+            <a href="mailto:apo.epsilon.conferencechair@gmail.com">
+              apo.epsilon.conferencechair@gmail.com
             </a>
           </p>
         </div>
