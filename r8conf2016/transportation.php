d68d78906c4cda5ba5539f9bc40f8a661669afd7
Moon "Moonman" Man <nge4445@truman.edu>
2015-09-26 20:02:07 -0700

    Update transportation.php

--- r8conf2016/transportation.php
+++ r8conf2016/transportation.php
@@ -1,21 +1,14 @@
-<?php
-require_once ('session.php');
-?>
-<!doctype html>
+
 <html>
 <head>
-    <?php require 'head.php';?>
+   poop
 </head>
 
-<body class="slide" data-type="background" data-speed="5">
-    <nav id="nav" role="navigation"></nav>
-    <div id="header"></div>
 
 <div class="row">
         <div class="large-10 medium-9 small-12 column large-centered medium-centered">
-            <?'editable_page.php'); ?>
-        </div>
-    </div>
+          </div>
     <div id="footer"></div>
 </body>
+ooooooooo</body>
 </html>
