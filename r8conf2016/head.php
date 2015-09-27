1050f07d2fee1bde02cbeca3dd48f2b1da244b1b
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-09-27 00:54:24 -0500

    Corrected path to site icon in head.php

--- r8conf2016/head.php
+++ r8conf2016/head.php
@@ -2,7 +2,7 @@
     <title>APO - Epsilon Chapter</title>
     <meta name="author" content="Dale Race">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link href="favicon.png" rel="shortcut icon">
+    <link href="../favicon.png" rel="shortcut icon">
     <link href="css/foundation.css" rel="stylesheet" type="text/css"/>
     <link href="css/screen.css" media="screen, projection" rel="stylesheet" type="text/css">
     <link href="css/jquery.fancybox.css" rel="stylesheet" type="text/css">
