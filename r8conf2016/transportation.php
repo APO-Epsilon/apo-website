5be3ffadaa55ac85dc16311b3e650092b95cae07
Moon "Moonman" Man <nge4445@truman.edu>
2015-09-26 20:02:40 -0700

    Update transportation.php

--- r8conf2016/transportation.php
+++ r8conf2016/transportation.php
@@ -1,14 +1,1 @@
 
-<html>
-<head>
-   poop
-</head>
-
-
-<div class="row">
-        <div class="large-10 medium-9 small-12 column large-centered medium-centered">
-          </div>
-    <div id="footer"></div>
-</body>
-ooooooooo</body>
-</html>
