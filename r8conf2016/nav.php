8163c8890a2f92bf5bb7480fe99e8ab713120a12
JustinTBayer <Justin.T.Bayer.dev@gmail.com>
2015-09-27 18:11:23 -0500

    Reworked r8conf2016 nav

--- r8conf2016/nav.php
+++ r8conf2016/nav.php
@@ -3,10 +3,21 @@
 	<a href="#" title="Hide navigation">Hide</a>
 	<ul class="clearfix">
 		<li><a href="index.php">Home</a></li>
-		<li><a href="schedule.php">Schedule</a></li>
 		<li><a href="register.php">Register</a></li>
+		<li>
+			<a href="schedule.php"><span>Events</span></a>
+			<ul>
+				<li><a href="schedule.php">Schedule</a></li>
+				<li><a href="jaredbox.php">The Jared Box</a></li>
+			</ul>
+		</li>
+        <li>
+        	<a href="transportation.php"><span>Logistics</span></a>
+        	<ul>
+        		<li><a href="transportation.php">Transportation</a></li>
+        	</ul>
+        </li>
 		<li><a href="conference_exec.php">Conference Committee</a></li>
-                <li><a href="transportation.php">Transportation</a></li>
-		<!--Last list item will be name if logged in>
+		<!--Last list item will be name if logged in-->
 	</ul>
     <!--<div id="righttriangle"></div>-->
