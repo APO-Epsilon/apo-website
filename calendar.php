56fdf181194ce3b698f12fbb8a659fa36d0fdc21
als44 <als4462@truman.edu>
2016-01-13 09:55:18 -0600

    added the new calendar

--- calendar.php
+++ calendar.php
@@ -19,7 +19,7 @@ require_once ('session.php');
 <div class="row">
         <div class="large-10 medium-9 small-12 column large-centered medium-centered">
             <br>
-            <iframe src="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=h84dpe9q6v9eft0vpgbbdmf2so%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=America%2FChicago" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+            <iframe src="https://www.google.com/calendar/embed?src=truman.edu_9gquk77vkfad4h1f4tna20k8nc%40group.calendar.google.com&ctz=America/Chicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
           </div>
 </div>
     <!-- Javascript method to include footer -->
