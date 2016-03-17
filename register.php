d4e34c1a03d9436a5ba2be5ee00dc5b40035082c
wjm4572 <wjm4572@truman.edu>
2016-03-17 17:30:18 -0600

    Update register.php

--- register.php
+++ register.php
@@ -85,6 +85,7 @@ require_once ('mysql_access.php');
             <option value="Spring">Spring</option>
           </select>
           <select name="pledgeyear">
+            <option value="2017">2017</option>
             <option value="2016">2016</option>
             <option value="2015">2015</option>
             <option value="2014">2014</option>
@@ -146,6 +147,7 @@ require_once ('mysql_access.php');
             <option value="2018">2018</option>
             <option value="2019">2019</option>
             <option value="2020">2020</option>
+            <option value="2021">2021</option>
           </select>
         <br>
 
