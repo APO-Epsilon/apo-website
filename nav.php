6c21bfc39699db94e96e63670b6b77e6fd9d8584
upandatom96 <a.callanan@sbcglobal.net>
2016-10-02 12:01:48 -0500

    Added 'links' to nav

--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
-  <!--<div id="lefttriangle"></div>-->
+<!--<div id="lefttriangle"></div>-->
     <a href="#nav" title="Show navigation">Show</a>
   <a href="#" title="Hide navigation">Hide</a>
   <ul class="clearfix">
@@ -48,6 +48,22 @@
                     <li><a href="check_hours.php">Check Hours</a></li>
                     <li><a href="edit_exec.php">Edit Exec</a></li>
                     <li><a href="member_statuses.php">Edit Members</a></li>
+                </ul>
+        </li>
+        <li>
+            <a href="?Links" aria-haspopup="true"><span>Links</span></a>
+                <ul>
+                    <li><a href="https://calendar.google.com/calendar/render?cid=u3q6udtat5aeia3jbfna05qga0@group.calendar.google.com#main_7">Google Calendar</a></li>
+                    <li><a href="https://drive.google.com/drive/folders/0B64sX_KlRJvYZG5Za3dDakVzYU0">Google Drive</a></li>   
+                    <li><a href="https://docs.google.com/spreadsheets/d/11J6o0GyKgN5c9_4CY68fO7Ne0u6hngnNez7_K19iE-M/edit#gid=0">Family Flower</a></li>
+                    <li><a href="https://docs.google.com/spreadsheets/d/1K_rUuRX0DNtQ930iT3nfpDwQxr53o_MFstckAl3AGlc/edit#gid=0">Large Service</a></li>
+                    <li><a href="https://docs.google.com/spreadsheets/d/1tGFRJCyG3HP0njM2O2MODeCQ9mVgvSVVQwxUfy4RSxQ/edit#gid=1343545188">Regular Service</a></li>
+                    <li><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                      <input type="hidden" name="cmd" value="_s-xclick">
+                      <input type="hidden" name="hosted_button_id" value="PEA3YS23SH8BY">
+                      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                      </form></li>
                 </ul>
         </li>
   </ul>
