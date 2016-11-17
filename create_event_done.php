e9d4add8eb38a685520c200f7d45ea7da2806d9f
upandatom96 <a.callanan@sbcglobal.net>
2016-11-17 10:51:22 -0600

    add details

--- create_event_done.php
+++ create_event_done.php
@@ -26,18 +26,23 @@ require_once('permissions.php');
 
 function show_active() {
 	include('mysql_access.php');
+	$user_id = $_SESSION['sessionID'];	
+	$aresponse=$db->query("SELECT status FROM contact_information WHERE id=$user_id");
+	$aresult=mysqli_fetch_array($aresponse);
+	$status = $aresult['status'];
+if ($status == 'Appointed' || $status == 'Elected') {
 	if (isset($_POST['submit']) && isset($_POST['event_name'])) 
 	{
-		$event_type = $_POST['event_type'];
-		$event_id = $_POST['event_id'];
-		$event_name = $_POST['event_name'];
-		$F_val = $_POST['L_val'];
-		$L_val = $_POST['F_val'];
-		$event_time = $_POST['event_time'];
-		$event_place = $_POST['event_place'];
-		$event_description = $_POST['event_description'];
-		$event_cap = $_POST['event_cap'];
-		$event_leader_id = $_POST['event_leader_id'];
+		$event_type = htmlspecialchars($_POST['event_type']);
+		$event_id = htmlspecialchars($_POST['event_id']);
+		$event_name = htmlspecialchars($_POST['event_name']);
+		$L_val = htmlspecialchars($_POST['L_val']);
+		$F_val = htmlspecialchars($_POST['F_val']);
+		$event_time = htmlspecialchars($_POST['event_time']);
+		$event_place = htmlspecialchars($_POST['event_place']);
+		$event_description = htmlspecialchars($_POST['event_description']);
+		$event_cap = htmlspecialchars($_POST['event_cap']);
+		$event_leader_id = htmlspecialchars($_POST['event_leader_id']);
 		$repeatable = 0;
 		if (isset($_POST['repeatable']))
 		{
@@ -49,7 +54,17 @@ function show_active() {
 			$required = 1;
 		}
 		
-		//check input for now
+		//add their signup to the database
+		$SQL = "INSERT INTO events_listing ";
+		$SQL = $SQL . "(event_type,event_id,event_name,L_val,F_val,event_time,event_place,event_description,event_cap,event_leader_id,repeatable,required)";
+		$SQL = $SQL . " VALUES ('";
+		$SQL = $SQL . $event_type . "'," . $event_id . ",'" . $event_name . "'," . $L_val . "," . $F_val . ",'" . $event_time . "','";
+		$SQL = $SQL . $event_place . "','" . $event_description . "'," . $event_cap . "," . $event_leader_id . ",";
+		$SQL = $SQL . $repeatable . "," . $required . ")";
+		$result = $db->query($SQL) or die("<a href='create_point_event.php'>Create an Event</a>");
+		echo "<h1>Event " . $event_name . " successfully created!</h1>";
+				
+		//check input
 		echo "Event Type: " . $event_type . '<br>';
 		echo "Event ID: " . $event_id . '<br>';
 		echo "Event Name: " . $event_name . '<br>';
@@ -62,16 +77,6 @@ function show_active() {
 		echo "Leader ID: " . $event_leader_id . '<br>';
 		echo "Repeatable: " . $repeatable . '<br>';
 		echo "Required: " . $required . '<br>';
-		
-		//Chapter event leader is the rec sec (so use this for chp events)
-			//$rresponse=$db->query("SELECT id FROM contact_information WHERE position='Recording Secretary'");
-			//$rresult=mysqli_fetch_array($rresponse);
-			//$rec_id = $rresult['id'];
-		
-		//add their signup to the database
-			//$SQL = "INSERT INTO events_listing (event_id,event_name,event_description,F_val,L_val,event_cap,event_type,repeatable,required,event_leader_id) VALUES (" . $event_id . ",'". $event_name . "','". $event_description ."',". $F_val . ",". $L_val . "," . $event_cap . ",'" . $event_type . "'," . $repeatable . "," . $required . "," . $leader_id . ")";
-			//$result = $db->query($SQL) or die("<a href='create_point_event.php'>Create an Event</a>");
-			//echo "<h1>Event " . $event_name . " successfully created!</h1>";
 	}
 	echo "<a href='create_event.php'>Create an Event</a><br>";
 	//display all events in a table
@@ -87,6 +92,10 @@ function show_active() {
 		echo "<td>" . $result['event_type'] . "</td></tr>";
 	}
 	echo "</table>";
+}
+else{
+	echo "Only exec can create events.";
+}
 }
 
 
