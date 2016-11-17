8b264ec8f68845028d8c60b3e56c8b5d9242204c
upandatom96 <a.callanan@sbcglobal.net>
2016-11-17 10:50:38 -0600

    update create event with details

--- create_event.php
+++ create_event.php
@@ -27,9 +27,9 @@ require_once('permissions.php');
 function display($event_type) {
 		include('mysql_access.php');
 		$user_id = $_SESSION['sessionID'];	
-		$response=$db->query("SELECT COUNT(event_id) FROM events_listing");
+		$response=$db->query("SELECT MAX(event_id) FROM events_listing");
 		$result=mysqli_fetch_array($response);
-		$next = $result['COUNT(event_id)'] + 1;
+		$next = $result['MAX(event_id)'] + 1;
 		if ($event_type=='Other')
 		{
 			?>
@@ -92,6 +92,7 @@ function display($event_type) {
 			<?php
 		}
 		else if ($event_type=='Taskforce') {
+			$task_desc = "Help an exec member!";
 			?>	
 			<h2>Create Taskforce Event</h2>
 			<form name="myform" action="create_event_done.php" method="post">
@@ -101,16 +102,16 @@ function display($event_type) {
 				<input type="text" name="event_name" required></p>
 				<p>Leadership Points:
 				<input type="number" step="0.25" name="L_val" value="0"></p>
+				<input type="hidden" step="0.25" name="F_val" value="0"></p>
 				<p>Date: (leave blank if not applicable)
 				<input type='date' name='event_time' value=0></p>
 				<p>Meeting Location:
 				<input type='text' name='event_place' value='Circle Drive'></p>
 				<p>Description:<p>
-				<input type="text" name="event_description"></p>
+				<input type="text" name="event_description" value=<?= $task_desc ?>></p>
 				<p>Event Cap: (use 0 if there is no cap)
 				<input type="number" name="event_cap" value="0"></p>
-				<p>Event Leader ID: (defaults to your ID)
-				<input type='number' name='event_leader_id' value=<?= $user_id ?>></p>
+				<input type='hidden' name='event_leader_id' value=<?= $user_id ?>></p>
 				<p>Repeatable?
 				<input type="checkbox" name="repeatable"></p>
 				<p>
@@ -119,6 +120,9 @@ function display($event_type) {
 			<?php
 		}
 		else if ($event_type=='Chapter') {
+			$rresponse=$db->query("SELECT id FROM contact_information WHERE position='Recording Secretary'");
+			$rresult=mysqli_fetch_array($rresponse);
+			$rec_id = $rresult['id'];
 			?>	
 			<h2>Create Chapter Event</h2>
 			<form name="myform" action="create_event_done.php" method="post">
@@ -136,8 +140,7 @@ function display($event_type) {
 				<input type="text" name="event_description"></p>
 				<p>Event Cap: (use 0 if there is no cap)
 				<input type="number" name="event_cap" value="0"></p>
-				<p>Event Leader ID: (defaults to your ID)
-				<input type='number' name='event_leader_id' value=<?= $user_id ?>></p>
+				<input type='hidden' name='event_leader_id' value=<?= $rec_id ?>></p>
 				<p>Repeatable?
 				<input type="checkbox" name="repeatable"></p>
 				<p>Required?
@@ -153,6 +156,12 @@ function show_active() {
 	?>
 	<h1> Event Creation Home </h1>
 		<?php
+	include('mysql_access.php');
+	$user_id = $_SESSION['sessionID'];	
+	$aresponse=$db->query("SELECT status FROM contact_information WHERE id=$user_id");
+	$aresult=mysqli_fetch_array($aresponse);
+	$status = $aresult['status'];
+	if ($status == 'Appointed' || $status == 'Elected') {
 		$page = null;
 		//sets event info section from selection
 		$event_type = "none";
@@ -160,8 +169,6 @@ function show_active() {
 		{
 			$event_type = $_POST['type_set'];
 		}
-		include('mysql_access.php');
-		$user_id = $_SESSION['sessionID'];	
 		$response=$db->query("SELECT COUNT(event_id) FROM events_listing");
 		$result=mysqli_fetch_array($response);
 		$next = $result['COUNT(event_id)'] + 1;
@@ -170,15 +177,21 @@ function show_active() {
 		<form name="myform" action="" method="post">
 		<select name='type_set' onchange="this.form.submit()">
 			<option value='null' selected>-- select one --</option>
-			<option value='Chapter'>Chapter</option>
+			<!-- <option value='Chapter'>Chapter</option> !-->
 			<option value='Taskforce'>Taskforce</option>
-			<option value='Large Service'>Large Service</option>
+			<!-- <option value='Large Service'>Large Service</option>!-->
 			<option value='Other'>Other</option>
 		</select>
 		</form>
 		<?php
 		display($event_type);
+	}
+	else 
+	{
+		echo "You cannot create events because you are not on Exec.";
+	}
 }
+
 
 
 ?>
